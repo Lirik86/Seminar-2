ab3fb894d8a40cf29f76ae2c5570d9c024ceee21
lirik <lightlirikus@yandex.ru>
2023-02-22 17:27:59 +0300

    1 задание

--- zadanie/Program.cs
+++ zadanie/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int promt(string msg)
+{
+    System.Console.Write($"{msg}");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+bool validate(int num)
+{
+    if (num >= 100 && num < 1000)
+    {
+        return true;
+    }
+    Console.WriteLine("ввели не верное число");
+    return false;
+}
+
+int value = promt("Введите 3-х значное число >");
+if (validate(value))
+{
+    int last = value % 10;
+    Console.WriteLine($"третий знак равен {last}");
+}
